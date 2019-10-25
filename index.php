be601c9b6d8ab5208a5968d9a2461d4f52235bfb
Gentech57 <hsalih1999@gmail.com>
2019-10-26 00:07:16 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<form action=‘index.php’ method="post">
+
+<input type="email" name="email" required="required"/>
+
+<input type="password" name="password"/>
+
+<input type="checkbox" name="remember_me" value="Remember me"/>
+
+<input type="submit" value="Submit"/>
+
+</form>
